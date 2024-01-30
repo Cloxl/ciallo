c5a26f9f4e3be7d91216d4f42877c9f02192c770
Cloxl <Cloxl2017@outlook.at>
2024-01-31 01:47:45 +0800

    fix canvas draw bug, add setting button

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,98 +1,141 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styles from '../css/audio.module.css'
-import { getRandomColor, getRandomDuration, getRandomFont, getRandomSize, getRandomText } from '../tools/getRandomEvents';
+import {getRandomColor, getRandomDuration, getRandomFont, getRandomSize, getRandomText} from '../tools/getRandomEvents';
+import { BarsOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 
 const AudioPlayer: React.FC = () => {
-  const [audioSrc, setAudioSrc] = useState<string | null>(null);
-  const [gifSrc, setGifSrc] = useState<string | null>(null);
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const [audioSrc, setAudioSrc] = useState<string | null>(null);
+    const [gifSrc, setGifSrc] = useState<string | null>(null);
+    const [maxTextsPerDraw, setMaxTextsPerDraw] = useState<number>(10);
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  useEffect(() => {
-    const audioURL = `${process.env.PUBLIC_URL}/audio/ciallo.aac`;
-    const gifURL = `${process.env.PUBLIC_URL}/imgs/gif/ciallo.gif`;
-    setAudioSrc(audioURL);
-    setGifSrc(gifURL);
+    useEffect(() => {
+        const audioURL = `${process.env.PUBLIC_URL}/audio/ciallo.aac`;
+        const gifURL = `${process.env.PUBLIC_URL}/imgs/gif/ciallo.gif`;
+        setAudioSrc(audioURL);
+        setGifSrc(gifURL);
 
-    const canvas = canvasRef.current;
-    if (canvas) {
-      canvas.width = canvas.clientWidth;
-      canvas.height = canvas.clientHeight;
-      const ctx = canvas.getContext('2d');
-      if (ctx) {
-        let texts: { content: any; color: any; font: any; size: any; duration: any; x: number; y: number; }[] = [];
-        let timer: string | number | NodeJS.Timeout | undefined;
-        let animationFrameId: number;
+        setTimeout(() => {
+            const canvas = canvasRef.current;
+            if (canvas) {
+                const ctx = canvas.getContext('2d');
+                if (ctx) {
+                    canvas.width = canvas.clientWidth;
+                    canvas.height = canvas.clientHeight;
+                    let texts: {
+                        content: any;
+                        color: any;
+                        font: any;
+                        size: any;
+                        duration: any;
+                        x: number;
+                        y: number;
+                    }[] = [];
+                    let timer: string | number | NodeJS.Timeout | undefined;
+                    let animationFrameId: number;
 
-        const maxTexts = 100;  // 设置最大文本量
+                    const maxTexts = 100;
 
-        const setupAnimation = () => {
-          if (timer) {
-            clearInterval(timer);
-          }
-          timer = setInterval(() => {
-            if (texts.length < maxTexts) {
-              const text = {
-                content: getRandomText(),
-                color: getRandomColor(),
-                font: getRandomFont(),
-                size: getRandomSize(),
-                duration: getRandomDuration(),
-                x: canvas.width,
-                y: Math.random() * canvas.height,
-              };
-              texts.push(text);
+                    const setupAnimation = () => {
+                        if (timer) {
+                            clearInterval(timer);
+                        }
+                        timer = setInterval(() => {
+                            if (texts.length < maxTexts) {
+                                for (let i = 0; i < maxTextsPerDraw; i++) {
+                                    const size = getRandomSize();
+                                    let y = Math.random() * canvas.height;
+                                    let duration = getRandomDuration();
+
+                                    if (maxTexts - texts.length < 40) {
+                                        duration += 4;
+                                    }
+                                    if (y - size < 0) {
+                                        y = size + Math.random() * (15 - 4) + 4;
+                                    }
+                                    const text = {
+                                        content: getRandomText(),
+                                        color: getRandomColor(),
+                                        font: getRandomFont(),
+                                        size: `${size}px`,
+                                        duration: duration,
+                                        x: canvas.width,
+                                        y: y,
+                                    };
+
+                                    texts.push(text);
+                                }
+                            }
+                        }, 1000);
+
+                        const animate = () => {
+                            ctx.clearRect(0, 0, canvas.width, canvas.height);
+                            texts = texts.filter(text => {
+                                if (text.x + ctx.measureText(text.content).width < 0) {
+                                    return false;  // 如果文本已经离开了画布，就移除这个文本
+                                } else {
+                                    ctx.font = `${text.size} ${text.font}`;
+                                    ctx.fillStyle = text.color;
+                                    ctx.fillText(text.content, text.x, text.y);
+                                    text.x -= text.duration;
+                                    return true;  // 如果文本还在画布上，就保留这个文本
+                                }
+                            });
+                            animationFrameId = requestAnimationFrame(animate);
+                        };
+                        animate();
+                    };
+
+                    const handleVisibilityChange = () => {
+                        if (document.hidden) {
+                            // 当文档不可见时，停止动画
+                            clearInterval(timer);
+                            cancelAnimationFrame(animationFrameId);
+                        } else {
+                            // 当文档变为可见时，重新开始动画
+                            setupAnimation();
+                        }
+                    };
+                    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+                    setupAnimation();
+
+
+                    return () => {
+                        // 清理工作
+                        clearInterval(timer);
+                        cancelAnimationFrame(animationFrameId);
+                        document.removeEventListener('visibilitychange', handleVisibilityChange);
+                    };
+                }
             }
-          }, 1000);
+        }, 0);
 
-          const animate = () => {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            texts = texts.filter(text => {
-              if (text.x + ctx.measureText(text.content).width < 0) {
-                return false;  // 如果文本已经离开了画布，就移除这个文本
-              } else {
-                ctx.font = `${text.size} ${text.font}`;
-                ctx.fillStyle = text.color;
-                ctx.fillText(text.content, text.x, text.y);
-                text.x -= text.duration;
-                return true;  // 如果文本还在画布上，就保留这个文本
-              }
-            });
-            animationFrameId = requestAnimationFrame(animate);
-          };
-          animate();
-        };
+    }, [maxTextsPerDraw, canvasRef]);
 
-        setupAnimation();
+    const handleClick = () => {
+        if (audioSrc) {
+            const audio = new Audio(audioSrc);
+            audio.play().then(r => r).catch(e => e);
+        }
+    };
 
-        return () => {
-          clearInterval(timer);
-          cancelAnimationFrame(animationFrameId);
-        };
-      }
+    if (!audioSrc || !gifSrc) {
+        return null;
     }
-  }, []);
 
-  const handleClick = () => {
-    if (audioSrc) {
-      const audio = new Audio(audioSrc);
-      audio.play().then(r => r).catch(e => e);
-    }
-  };
-
-  if (!audioSrc || !gifSrc) {
-    return null;
-  }
-
-  return (
-    <div onClick={handleClick} className={styles.page}>
-      <div>
-        <canvas ref={canvasRef} className={styles.CialloCanvas} />
-      </div>
-      <div className={styles.CenterCialloContainer}>
-        <img src={gifSrc} alt="ciallo gif" />
-      </div>
-    </div>
-  );
+    return (
+        <div onClick={handleClick} className={styles.page}>
+            <div>
+                <canvas ref={canvasRef} className={styles.CialloCanvas}/>
+            </div>
+            <div className={styles.CenterCialloContainer}>
+                <img src={gifSrc} alt="ciallo gif"/>
+            </div>
+           <Button type="primary" icon={<BarsOutlined />} />
+        </div>
+    );
 };
 
 export default AudioPlayer;
--- src/tools/getRandomEvents.tsx
+++ src/tools/getRandomEvents.tsx
@@ -1,21 +1,21 @@
 const fonts = ["Arial, sans-serif", "Georgia, serif", "Times New Roman, serif"];
 
 export const getRandomFont = () => {
-  return fonts[Math.floor(Math.random() * fonts.length)];
+    return fonts[Math.floor(Math.random() * fonts.length)];
 };
 
 export const getRandomText = () => {
-  return ["Ciallo～(∠・ω< )⌒★", "Ciallo～(∠・ω< )⌒☆"][Math.floor(Math.random() * 2)];
+    return ["Ciallo～(∠・ω< )⌒★", "Ciallo～(∠・ω< )⌒☆"][Math.floor(Math.random() * 2)];
 };
 
 export const getRandomColor = () => {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 
 export const getRandomDuration = () => {
-  return Math.floor(Math.random() * 10);
+    return Math.floor(Math.random() * 9) + 1;
 };
 
 export const getRandomSize = () => {
-  return Math.floor(Math.random() * 40 + 10) + "px"; // 生成10到50之间的随机数作为文字大小
+    return Math.floor(Math.random() * 40 + 10);
 };
